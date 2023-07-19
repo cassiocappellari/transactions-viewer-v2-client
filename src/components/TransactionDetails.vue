dc14a99ab12f120fe7b2c41e8758b54f1cb1213d
cassiocappellari <cassiocappellari@gmail.com>
2023-07-19 16:58:30 +0200

    feat: change the transactions details order

--- src/components/TransactionDetails.vue
+++ src/components/TransactionDetails.vue
@@ -7,28 +7,28 @@
       <table align="center" border="1">
         <thead>
           <tr>
-            <th>Account</th>
+            <th>Transaction Date</th>
             <th>Description</th>
-            <th>Category</th>
-            <th>Reference</th>
             <th>Currency</th>
             <th>Amount</th>
+            <th>Account</th>
+            <th>Category</th>
+            <th>Reference</th>
             <th>Status</th>
-            <th>Transaction Date</th>
             <th>Created At</th>
             <th>Updated At</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>{{ transaction.account }}</td>
+            <td>{{ transaction.transactionDate }}</td>
             <td>{{ transaction.description }}</td>
-            <td>{{ transaction.category }}</td>
-            <td>{{ transaction.reference }}</td>
             <td>{{ transaction.currency }}</td>
             <td>{{ transaction.amount }}</td>
+            <td>{{ transaction.account }}</td>
+            <td>{{ transaction.category }}</td>
+            <td>{{ transaction.reference }}</td>
             <td>{{ transaction.status }}</td>
-            <td>{{ transaction.transactionDate }}</td>
             <td>{{ transaction.createdAt }}</td>
             <td>{{ transaction.updatedAt }}</td>
           </tr>
